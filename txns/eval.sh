9dda30dd9d6d6cfee6303ed5b3bf4381b4415f3f
WeirdFlexButOk7 <vardanjsp1515@gmail.com>
2026-01-15 18:03:36 +0530

    6a: Single-Node, Totally-Available Transactions

---
+++ txns/eval.sh
@@ -0,0 +1,1 @@
+../maelstrom/maelstrom test -w txn-rw-register --bin txns --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
---
+++ txns/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "context"
+
+    maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+    n := maelstrom.NewNode()
+    kv := maelstrom.NewSeqKV(n)
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    n.Handle("txn", func(msg maelstrom.Message) error {
+        var body map[string]any
+        if err := json.Unmarshal(msg.Body, &body); err != nil {
+            return err
+        }
+
+        resp := map[string]any{
+            "type": "txn_ok",
+            "txn": [][]any{},
+        }
+
+        txns := body["txn"].([]any)
+        out := resp["txn"].([][]any)
+
+        for _, rawArr := range txns {
+            arr := rawArr.([]any)
+            op := arr[0].(string)
+            key := string(int(arr[1].(float64)))
+
+            if op == "w" {
+                intValue := int(arr[2].(float64))
+                kv.Write(ctx, key, intValue);
+            } else {
+                intValue, _ := kv.ReadInt(ctx, key)
+                arr[2] = intValue
+            }
+
+            out = append(out, arr)
+
+        }
+
+        resp["txn"] = out
+
+        return n.Reply(msg, resp)
+    })
+
+    if err := n.Run(); err != nil {
+        log.Fatal(err)
+    }
+
+}
